c246cc7ce29a631dff7fd28aa527deb4dabe2000
Robert Campbell <rrc@robert-campbell.com>
2012-06-09 20:37:38 +0200

    initial commit

---
+++ src/kyts/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns kyts.views.welcome
+  (:require [kyts.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to kyts foo"]))
